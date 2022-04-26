fce1b6f57e8ce70a7dd9a2eb419c54f498675b81
jarekTerejko <j.terejko@gmail.com>
2022-04-26 23:44:40 +0200

    create HeadingSecondary

---
+++ src/components/HeadingSecondary/HeadingSecondary.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const HeadingSecondary = styled.h2`
+  font-weight: 900;
+  text-transform: uppercase;
+  line-height: 1;
+  font-family: var(--font-heading);
+  font-size: 5rem;
+  max-width: 30rem;
+  margin-bottom: 4rem;
+`;
