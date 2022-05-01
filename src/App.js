9bdee30188274a985b6532e93b191ddc19e39b05
jarekTerejko <j.terejko@gmail.com>
2022-05-01 06:52:46 +0200

    create and add Location page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Location from "./pages/Location";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="location" element={<Location />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
