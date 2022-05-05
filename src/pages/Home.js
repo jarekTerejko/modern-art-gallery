a5d3530a00b9e4c8df860803f6f4d14c45600091
jarekTerejko <j.terejko@gmail.com>
2022-05-05 08:29:51 +0200

    create and add ScrollToTop component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import Gallery from "../components/Gallery";
 import Header from "../components/Header";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 
 const Home = () => {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Gallery />
     </>
--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,9 +1,11 @@
 import LocationDetails from "../components/LocationDetails";
 import LocationMap from "../components/Map";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 
 const Location = () => {
   return (
     <>
+      <ScrollToTop />
       <LocationMap />
       <LocationDetails />
     </>
